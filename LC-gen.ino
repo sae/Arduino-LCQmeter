b5f2b24f8e6766326c9f987d517d38d2af0d33f2
Alexandr Sgbnev <sgibnev001@gmail.com>
2015-02-08 22:15:34 +0600

    Update LC-gen.ino
    change ctl to D2, change D2 control. now only one resistor and LC )

--- LC-gen.ino
+++ LC-gen.ino
@@ -12,7 +12,7 @@ http://tel-spb.ru/lc.html
 
 #include "analogComp.h"
 
-#define ctl 12
+#define ctl 2
 #define led 13
 
 int rise=0;
@@ -40,9 +40,43 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   //if no pulses, make a start impulse
   if (rise==0) {
-   digitalWrite(ctl, HIGH);
-   rise++;//make something useless )
-   digitalWrite(ctl, LOW);
+    pulse();
   }
  delay(1000);              // wait for a second
+}
+
+/**
+not so clear "magic" with interrupt
+we need to block int's when make a power impulse
+
+comparator is connected directly to capacitor
+*/
+boolean waitforRise=false;
+
+//this function is called when comparator change status
+//as set in ACSR register
+void comparatorInt() {
+  
+  if (waitforRise) { //if we waiting for rise
+    ACSR &= ~(1<<ACIS0);//set interrupt for falling
+    ACSR |= (1<<ACIS1);
+    waitforRise=false;
+    rise++;
+  } else { //if we waiting for fall
+   ACSR &= ~(1<<ACIE); //disable interrupt
+    //impulse to support an oscillations
+   pulse();
+   ACSR |= (1<<ACIE); //enable interrupt
+   ACSR |= ((1<<ACIS1) | (1<<ACIS0)); //set interrupt for rising
+   waitforRise=true;
+  }
+}
+
+//send power impulse to LC
+void pulse() {
+   pinMode(ctl, OUTPUT);
+   digitalWrite(ctl, HIGH);
+   rise++;//make something useless )
+   //digitalWrite(ctl, LOW);
+   pinMode(ctl, INPUT);
 }
